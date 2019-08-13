845a27a4d47fce6d6bf1926f7f33f90b960aa6f9
Gabriel Enslein <gabrielenslein@Gabriels-MBP-2.lan1>
2019-08-13 18:02:32 -0400

    fix slns 1 and two to include waitgroup work

--- ninja-level-nine/ninja_nine_sln.1.go
+++ ninja-level-nine/ninja_nine_sln.1.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
 
+	var wg sync.WaitGroup
+	wg.Add(2)
+
 	go func() {
 		fmt.Println("First goroutine, woohoo!")
+		wg.Done()
 	}()
 
 	go func() {
 		fmt.Println("Second goroutine, yeehaw!")
+		wg.Done()
 	}()
 
 	fmt.Println("Hello Playground")
--- ninja-level-nine/ninja_nine_sln.2.go
+++ ninja-level-nine/ninja_nine_sln.2.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 type person struct {
@@ -24,12 +25,17 @@ func saySomething(h human) {
 
 func main() {
 
+	var wg sync.WaitGroup
+	wg.Add(2)
+
 	go func() {
 		fmt.Println("First goroutine, woohoo!")
+		wg.Done()
 	}()
 
 	go func() {
 		fmt.Println("Second goroutine, yeehaw!")
+		wg.Done()
 	}()
 
 	fmt.Println("Hello Playground")
