452a01cba5f3b3abef85ee6f24ecf4fd7c2c3aee
Gabriel Enslein <genslein@genslein-ltm.internal.salesforce.com>
2019-08-14 17:52:48 -0400

    add buffered channel solution

--- ninja-level-ten/ninja_ten_sln.1.go
+++ ninja-level-ten/ninja_ten_sln.1.go
@@ -7,9 +7,16 @@ import (
 func main() {
 	c := make(chan int)
 
+	bc := make(chan int, 1)
+
 	go func() {
 		c <- 42
 	}()
 
+	go func() {
+		bc <- 42
+	}()
+
 	fmt.Println(<-c)
+	fmt.Println(<-bc)
 }
