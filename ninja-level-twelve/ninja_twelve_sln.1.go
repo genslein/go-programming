0b28f2b6cba66b05a45c8a31d4eaefc7010ec238
Gabriel Enslein <genslein@genslein-ltm.internal.salesforce.com>
2019-08-20 20:12:01 -0400

    add dog solution ninja twelve

---
+++ ninja-level-twelve/ninja_twelve_sln.1.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/genslein/go-programming/ninja-level-twelve/dog"
+)
+
+func main() {
+	fmt.Println("Made it back to main with doggo", dog.Years(8))
+}
