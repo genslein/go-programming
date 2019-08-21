6b6c8a1e0c8484812ff773fd01891307910ba4e5
Gabriel Enslein <genslein@genslein-ltm.internal.salesforce.com>
2019-08-20 20:15:45 -0400

    fix with periods on comments

--- ninja-level-twelve/dog/dog.go
+++ ninja-level-twelve/dog/dog.go
@@ -1,7 +1,7 @@
-// Package dog implements the dog year conversion
+// Package dog implements the dog year conversion.
 package dog
 
-// Years converts the dog's human years to dog years
+// Years converts the dog's human years to dog years.
 func Years(y int) int {
 	return y * 7
 }
